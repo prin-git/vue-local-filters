28327f4c69e7a840ea654f1a1fa6c47a7ff76396
React, Vue, GraphQL , JS, CSS3, HTML5 <54697180+prin-git@users.noreply.github.com>
2023-05-27 11:24:53 -0500

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>{{ text | toUppercase }}</div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      text: "hello",
+    };
+  },
+  filters: {
+    toUppercase(value) {
+      return value.toUpperCase();
+    },
+  },
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
